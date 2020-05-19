381be05432da4d53494c60381b5db4b94678b64f
Peter Siska <psiska@inventage.com>
2020-05-19 11:10:23 +0200

    Trim trailing spaces

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
   stories: ['../stories/**/*.stories.{js,md,mdx}'],
-  addons: [	
-    'storybook-prebuilt/addon-knobs/register.js',	
-    'storybook-prebuilt/addon-docs/register.js',	
+  addons: [
+    'storybook-prebuilt/addon-knobs/register.js',
+    'storybook-prebuilt/addon-docs/register.js',
     'storybook-prebuilt/addon-viewport/register.js',
   ],
   esDevServer: {
