93a802ea7e185a8b0c88b50142d0c23fea698b3e
Peter Siska <psiska@inventage.com>
2020-05-20 15:01:59 +0200

    Fix tests

--- components/portal-card/test/portal-card.test.js
+++ components/portal-card/test/portal-card.test.js
@@ -6,15 +6,7 @@ describe('PortalCard', () => {
   it('has a default title "Hey there" and counter 5', async () => {
     const el = await fixture(html` <portal-card></portal-card> `);
 
-    expect(el.title).to.equal('Hey there');
-    expect(el.counter).to.equal(5);
-  });
-
-  it('increases the counter on button click', async () => {
-    const el = await fixture(html` <portal-card></portal-card> `);
-    el.shadowRoot.querySelector('button').click();
-
-    expect(el.counter).to.equal(6);
+    expect(el.title).to.equal('I am a glorious title');
   });
 
   it('can override the title via attribute', async () => {
