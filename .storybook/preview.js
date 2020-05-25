3edab5a89a1ebc7cb6959a089b8aa385c1f1c615
Peter Siska <psiska@inventage.com>
2020-05-25 15:31:06 +0200

    Add more documentation links

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -28,6 +28,8 @@ function run() {
     },
     // Storybook Docs
     // @see https://github.com/storybookjs/storybook/tree/master/addons/docs
+    // @see https://github.com/storybookjs/storybook/blob/next/addons/docs/common/README.md#iframe-height
+    // @see https://github.com/storybookjs/storybook/blob/next/addons/docs/web-components/README.md
     docs: {
       iframeHeight: '200px',
     },
