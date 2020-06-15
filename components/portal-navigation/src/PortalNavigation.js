585bc1f20bd390ae051a818e423cae757bfe3e08
Peter Siska <psiska@inventage.com>
2020-06-15 11:25:49 +0200

    Fix typings, we have to export CSSResultArray as well

--- components/portal-navigation/src/PortalNavigation.js
+++ components/portal-navigation/src/PortalNavigation.js
@@ -1,8 +1,15 @@
+/**
+ * @typedef { import("lit-element").CSSResult } CSSResult
+ * @typedef { import("lit-element").CSSResultArray } CSSResultArray
+ */
 import { LitElement } from 'lit-element';
 import { nothing } from 'lit-html';
 import { baseStyles } from '../../helpers/baseStyles.js';
 
 export class PortalNavigation extends LitElement {
+  /**
+   * @returns {CSSResult|CSSResultArray}
+   */
   static get styles() {
     return baseStyles;
   }
