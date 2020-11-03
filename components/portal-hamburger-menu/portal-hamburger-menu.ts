95a91b016e7c5ea3dbccc50f6337b2939134ffaa
Peter Siska <psiska@inventage.com>
2020-11-03 17:41:30 +0100

    Add missing declarations in HTMLElementTagNameMap

--- components/portal-hamburger-menu/portal-hamburger-menu.ts
+++ components/portal-hamburger-menu/portal-hamburger-menu.ts
@@ -1,3 +1,9 @@
 import { PortalHamburgerMenu } from './src/PortalHamburgerMenu';
 
 customElements.define('portal-hamburger-menu', PortalHamburgerMenu);
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'portal-hamburger-menu': PortalHamburgerMenu;
+  }
+}
--- components/portal-navigation/portal-navigation.ts
+++ components/portal-navigation/portal-navigation.ts
@@ -1,3 +1,9 @@
 import { PortalNavigation } from './src/PortalNavigation';
 
 customElements.define('portal-navigation', PortalNavigation);
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'portal-navigation': PortalNavigation;
+  }
+}
